93f983b3136fdeb4282485028367aa90233ae506
alanrgan <alanrgan@gmail.com>
2016-02-03 00:01:17 -0600

    Updated formatting

--- Tree.java
+++ Tree.java
@@ -13,8 +13,7 @@ public class Tree<T> {
 	private List<Tree<T>> children = new ArrayList<Tree<T>>();
     private HashMap<T, Tree<T>> locate = new HashMap<T, Tree<T>>();
         
-    public Tree(T data)
-    {
+    public Tree(T data) {
         setData(data);
     }
         
@@ -22,16 +21,14 @@ public class Tree<T> {
      * Add a node under a specified root node.
      * If root node does not exist, it will be created.
      */
-    public void addNode(T root, T data)
-    {
+    public void addNode(T root, T data) {
         if(locate.containsKey(root))
         	locate.get(root).addNode(data);
         else
         	addNode(root).addNode(data);
     }
         
-    public Tree<T> addNode(T data)
-    {
+    public Tree<T> addNode(T data) {
         Tree<T> node = new Tree<T>(data);
         children.add(node);
         node.parent = this;
@@ -40,8 +37,7 @@ public class Tree<T> {
         return node;
     }
     
-    public Tree<T> setAsParent(T parentRoot)
-    {
+    public Tree<T> setAsParent(T parentRoot) {
     	Tree<T> t = new Tree<T>(parentRoot);
     	t.children.add(this);
     	this.parent = t;
@@ -51,24 +47,20 @@ public class Tree<T> {
     	return t;
     }
     
-    public void setData(T data)
-    {
+    public void setData(T data) {
     	this.data = data;
     	locate.put(data, this);
     }
         
-    public T getData()
-    {
+    public T getData() {
         return data;
     }
         
-    public Tree<T> getParent()
-    {
+    public Tree<T> getParent() {
       	return parent;
     }
         
-    public Tree<T> getTree(T element)
-    {
+    public Tree<T> getTree(T element) {
         return locate.get(element);
     }
         
@@ -79,14 +71,11 @@ public class Tree<T> {
      * @param  root the value of the head of the subtree to retrieve 
      * @return 		a list of the values of the immediate children of the root node
      */
-    public List<T> getSuccessors(T root)
-    {
+    public List<T> getSuccessors(T root) {
         List<T> successors = new ArrayList<T>();
         Tree<T> tree = locate.get(root);
-        if(tree != null)
-        {
-        	for(Tree<T> node : tree.children)
-        	{
+        if(tree != null) {
+        	for(Tree<T> node : tree.children) {
         		successors.add(node.getData());
         	}
         }
@@ -100,15 +89,12 @@ public class Tree<T> {
      * @param  root the value of the highest node of reference
      * @return 		a list containing the values of the subleaves
      */
-    public List<T> getSubLeafs(T root)
-    {
+    public List<T> getSubLeafs(T root) {
     	List<T> subleafs = new ArrayList<T>();
     	Tree<T> tree = locate.get(root);
     	
-    	if(tree != null)
-    	{
-    		if(!tree.hasChildren())
-    		{
+    	if(tree != null) {
+    		if(!tree.hasChildren()) {
     			subleafs.add(tree.getData());
     			return subleafs;
     		}
@@ -116,16 +102,13 @@ public class Tree<T> {
     			subleafs.add(tree.getData());
     		
     		for(Tree<T> node : tree.children)
-    		{
     			subleafs.addAll(getSubLeafs(node.getData()));
-    		}
     	}
     	
     	return subleafs;
     }
         
-    public List<Tree<T>> getChildNodes()
-    {
+    public List<Tree<T>> getChildNodes() {
      	return children;
     }
     
@@ -134,8 +117,7 @@ public class Tree<T> {
      * 
      * @param element the element to be removed
      */
-    public void remove(T element)
-    {
+    public void remove(T element) {
     	Tree<T> removeElement = getTree(element);
     	
     	recurseRemove(this, removeElement);
@@ -143,38 +125,31 @@ public class Tree<T> {
     	locate.remove(element);
     	
     	//Remove all instances of child nodes from the lookup array.
-    	for(Tree<T> tree : removeElement.getChildNodes())
-    	{
+    	for(Tree<T> tree : removeElement.getChildNodes()) {
 			locate.remove(tree.getData());
     	}
     }
     
-    private void recurseRemove(Tree<T> t, Tree<T> element)
-    {
-    	if(t.children.isEmpty() == false) // Checks to see if the current node has any children.
-    	{
+    private void recurseRemove(Tree<T> t, Tree<T> element) {
+    	if(t.children.isEmpty() == false) { // Checks to see if the current node has any children.
     		t.children.remove(element);
     		for(Tree<T> subTree : t.children)	// Traverses through each child node and removes all instances of the element.
     			recurseRemove(subTree, element);
     	}
     }
     
-    public void clear()
-    {
+    public void clear() {
     	clear(this, children);
         children.clear();
     }
     
-    private void clear(Collection<Tree<T>> clearList)
-    {
+    private void clear(Collection<Tree<T>> clearList) {
     	for(Tree<T> t : clearList)
     		locate.remove(t.getData());
     }
     
-    private void clear(Tree<T> t, Collection<Tree<T>> clearList)
-    {
-    	if(t.getParent() != null)
-    	{
+    private void clear(Tree<T> t, Collection<Tree<T>> clearList) {
+    	if(t.getParent() != null) {
     		t.getParent().clear(clearList);
     		clear(t.getParent(), clearList);
     	}
@@ -183,18 +158,14 @@ public class Tree<T> {
     /*
      * Returns the highest number of nodes in a level of the tree
      */
-    public int maxWidth()
-    {
+    public int maxWidth() {
     	List<Tree<T>> CurrNodes = new ArrayList<Tree<T>>();
     	CurrNodes.add(this);
     	int maxWidth = 1;
     	List<Tree<T>> NextNodes = new ArrayList<Tree<T>>();
-    	while(!CurrNodes.isEmpty())
-    	{
+    	while(!CurrNodes.isEmpty()) {
     		for(Tree<T> t : CurrNodes)
-    		{
     			NextNodes.addAll(t.getChildNodes());
-    		}
     		if(NextNodes.isEmpty())
     			break;
     		if(NextNodes.size() > maxWidth)
@@ -212,8 +183,7 @@ public class Tree<T> {
      * Returns the max height of a tree node
      * Recursive implementation 
      */
-    public int getHeight(Tree<T> t, int num, int max)
-    {
+    public int getHeight(Tree<T> t, int num, int max) {
     	List<Tree<T>> childNodes = t.children;
     	if(t.getChildNodes().isEmpty())
     		return num + 1;
@@ -221,15 +191,12 @@ public class Tree<T> {
     		max = num;
     	
     	for(int i = 0; i < childNodes.size(); i++)
-    	{
     		max = Math.max(max, getHeight(childNodes.get(i), num + 1, max));
-    	}
     	
     	return max;
     }
     
-    public boolean hasChildren()
-    {
+    public boolean hasChildren() {
     	return !children.isEmpty();
     }
         
