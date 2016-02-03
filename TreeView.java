97bc3765b546ec536b3e6cb53fe7e13cd8d3551d
alanrgan <alanrgan@gmail.com>
2016-02-02 23:58:37 -0600

    Updated formatting

--- TreeView.java
+++ TreeView.java
@@ -4,16 +4,13 @@ import javafx.application.Application;
 
 public abstract class TreeView extends Application {
 	
-	public static int getTreeHeight(Tree<String> tree)
-	{
+	public static int getTreeHeight(Tree<String> tree) {
 		return tree.getHeight(tree, 0, Integer.MIN_VALUE);
 	}
 	
-	public static int getNumberOfTabs(char[] charArray)
-	{
+	public static int getNumberOfTabs(char[] charArray) {
 		int tabNum = 0;
-		for(char c : charArray)
-		{
+		for(char c : charArray) {
 			if(c == '\t')
 				tabNum++;
 			else
